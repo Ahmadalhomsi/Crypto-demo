d85a0033b70b98d29d211da3c36ec53010a0b36d
Ahmadalhomsi <tarikuka.aa@gmail.com>
2025-04-29 22:31:58 +0300

    Template update

---
+++ Quiz2_GRUP_5.py
@@ -0,0 +1,21 @@
+# QUIZ 2
+
+# Grup 5
+
+# 210201140
+# Ahmad Alhomsi
+
+# 210201126
+# Ebrar Mustafa AÇIKYOL
+
+# 210201120
+# Cem Korkmaz
+
+
+def main():
+    print("Welcome to Quiz 2 - Grup 5")
+    # CODE HERE
+
+
+if __name__ == "__main__":
+    main()
